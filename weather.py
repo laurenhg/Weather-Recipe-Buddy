b3c6c6860154ee1ff36428a3cc518eea4a059589
laurenhg <laurenhg@gmail.com>
2024-11-19 12:32:49 +0100

    weather.py

---
+++ weather.py
@@ -0,0 +1,25 @@
+import requests
+
+def get_weather(city):
+    api_key = "182ba813dcb21cfb58a884cb7ef2259f"
+    url = f"http://api.weatherstack.com/current"
+    params = {
+        "access_key": api_key,
+        "query": city
+    }
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        data = response.json()
+        if "current" in data:
+        weather = {
+            "temperature": data["current"]["temperature"],
+            "description": data["current"]["weather_descriptions"][0],
+            "city": data["location"]["name"]
+        }
+        return weather
+    else: print ("Error: unable to retrieve weather data.")
+    return none
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching weather data: {e}")
+        return None

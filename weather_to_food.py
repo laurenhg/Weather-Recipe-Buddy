59702b098dc1d061279ad914c18fb99bfd09272a
laurenhg <laurenhg@gmail.com>
2024-11-19 16:42:00 +0100

    updates to weather to food

--- weather_to_food.py
+++ weather_to_food.py
@@ -1,7 +1,12 @@
-def map_weather_to_food(weather):
-    if weather["temperature"] <10:
+def suggest_recipe_based_on_weather(weather):
+    temp = weather["temperature"]
+    description = weather["description"].lower()
+
+    if "rain" in description or "drizzle" in description:
         return "soup"
-    elif 10 <= weather["temperature"] <= 25:
+    elif temp < 10:
         return "comfort food"
+    elif temp > 25:
+        return "salads"
     else:
-        return "salad"
+        return "quick meals"

36c4c5468072faf7389b91ba2fd34eb7d1e41e62
laurenhg <laurenhg@gmail.com>
2024-11-20 16:28:01 +0100

    updates to loop

--- menu.py
+++ menu.py
@@ -1,11 +1,15 @@
 def display_menu():
-    print("n\Weather-Recipe Buddy")
-    print("1. Get Current Weather and Recipe Suggestions")
-    print ("2. View Saves Recipes")
-    print ("3. Exit")
-    try:
+    while True:
+        print("\nWeather-Recipe Buddy")
+        print("1. Get Current Weather and Recipe Suggestions")
+        print("2. View Saved Recipes")
+        print("3. Exit")
 
-        return int(input("Enter your choice: "))
-    except ValueError:
-        print("Invalid input. Please enter a number.")
-        return 0
+        try:
+            choice = int(input("Enter your choice: "))
+            if choice in [1, 2, 3]:
+                return choice
+            else:
+                print("Invalid choice. Please select 1, 2, or 3.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")

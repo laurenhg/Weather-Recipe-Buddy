51f21a27d178cdd869143b153cbc95991423693f
laurenhg <laurenhg@gmail.com>
2024-11-20 16:31:23 +0100

    simpler version

--- menu.py
+++ menu.py
@@ -1,15 +1,10 @@
 def display_menu():
-    while True:
-        print("\nWeather-Recipe Buddy")
-        print("1. Get Current Weather and Recipe Suggestions")
-        print("2. View Saved Recipes")
-        print("3. Exit")
-
-        try:
-            choice = int(input("Enter your choice: "))
-            if choice in [1, 2, 3]:
-                return choice
-            else:
-                print("Invalid choice. Please select 1, 2, or 3.")
-        except ValueError:
-            print("Invalid input. Please enter a number.")
+    print("\nWeather-Recipe Buddy")
+    print("1. Get Current Weather and Recipe Suggestions")
+    print("2. View Saved Recipes")
+    print("3. Exit")
+    try:
+        return int(input("Enter your choice: "))
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+        return 0

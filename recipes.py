cf6865f86ab09bf2e42f5e308d11ac7037364993
laurenhg <laurenhg@gmail.com>
2024-11-20 14:40:32 +0100

    secret api key

--- recipes.py
+++ recipes.py
@@ -1,11 +1,19 @@
 import requests
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 def get_recipes(food_category):
-    api_key = "YOUR_SPOONACULAR_API_KEY"  # Replace with your Spoonacular API key
+    api_key = os.getenv("SPOONACULAR_API_KEY")
+    if not api_key:
+        print("Error: SPOONACULAR_API_KEY is not set in the environment variables.")
+        return []
+
+
     url = "https://api.spoonacular.com/recipes/complexSearch"
     params = {
         "query": food_category,
-        "number": 5,
+        "number": 5,  # Fetch 5 recipes
         "apiKey": api_key
     }
 

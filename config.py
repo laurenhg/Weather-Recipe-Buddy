47d3f23b84f0b84e64fb9afc0ddb4c9fafe0776e
laurenhg <laurenhg@gmail.com>
2025-11-24 18:04:20 +0100

    Update config.py to load environment variables

--- config.py
+++ config.py
@@ -3,4 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
 SPOONACULAR_API_KEY = os.getenv("SPOONACULAR_API_KEY")
+SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
